f8a28157a1b6469dc5d75c59a9f1d8948b02ca73
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-10-24 21:20:35 -0600

    Routes and error handler

--- sat-api/app.py
+++ sat-api/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from routes.routes import application
+from controllers.ResponseController import onError
 
 app = Flask(__name__)
-
 app.register_blueprint(application)
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return onError('The method is not allowed for the requested URL.', 405)
 
 
 app.run()
--- sat-api/routes/routes.py
+++ sat-api/routes/routes.py
@@ -2,10 +2,40 @@ from flask import Blueprint, Response, request
 from config import API_BASE_ROUTE
 from controllers.ResponseController import onError, onSuccess
 
-application = Blueprint('route', __name__)
+application = Blueprint('routes', __name__)
 
-@application.route(f'{API_BASE_ROUTE}/')
-def hello():
+@application.route(f'{API_BASE_ROUTE}/ConsultaDatos')
+def consulta_datos():
+    if request.method == 'GET':
+        return onSuccess({
+            'success' : True
+        })
+    else:
+        return onError('The method is not allowed for the requested URL.', 405)
+
+@application.route(f'{API_BASE_ROUTE}/ResumenIva')
+def resumen_iva():
+    if request.method == 'GET':
+        return onSuccess({
+            'success' : True
+        })
+    else:
+        return onError('The method is not allowed for the requested URL.', 405)
+
+@application.route(f'{API_BASE_ROUTE}/ResumenRango')
+def resumen_rango():
+    return onSuccess({
+        'success' : True
+    })
+
+@application.route(f'{API_BASE_ROUTE}/Grafica')
+def grafica():
+    return onSuccess({
+        'success' : True
+    })
+
+@application.route(f'{API_BASE_ROUTE}/Procesar', methods = ['POST'])
+def procesar():
     return onSuccess({
         'success' : True
     })
