87c7f5da6999722e6ec8282d150e1f1e8d814663
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-11-03 00:11:15 -0600

    Load file

--- sat/frontend/views.py
+++ sat/frontend/views.py
@@ -9,10 +9,18 @@ def home(request):
     return render(request, 'home.html')
 
 def load(request):
-    if request.method == 'POST' and request.FILES['xml-file']:
+    if request.method == 'POST' and request.FILES.get('xml-file'):
         file = request.FILES['xml-file']
         
         files = {"file" : file}
         response = requests.post('http://127.0.0.1:5000/api/v1/Procesar', files = files)
-        print(response.json())
-    return render(request, 'home.html')
+        
+        if response.status_code == 200:
+            response_info = requests.get('http://127.0.0.1:5000/api/v1/ConsultaDatos')
+            file.seek(0)
+            file_readed = file.read().decode()
+            context = {
+                'salida' : response_info.json()['data'],
+                'entrada' : file_readed
+            }
+    return render(request, 'home.html', context = context)
