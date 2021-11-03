e682ac4f14cec1719d32fa2bc3914744a36b3f32
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-11-02 23:21:01 -0600

    Upload file

--- sat/frontend/views.py
+++ sat/frontend/views.py
@@ -1,3 +1,5 @@
+import requests
+
 from django.http import HttpResponse
 
 from django.shortcuts import render
@@ -7,4 +9,10 @@ def home(request):
     return render(request, 'home.html')
 
 def load(request):
+    if request.method == 'POST' and request.FILES['xml-file']:
+        file = request.FILES['xml-file']
+        
+        files = {"file" : file}
+        response = requests.post('http://127.0.0.1:5000/api/v1/Procesar', files = files)
+        print(response.json())
     return render(request, 'home.html')
